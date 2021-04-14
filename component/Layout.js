9f9639b74e701dfd482a92fdd5273c26ffdf64e0
Carlos Filgueira <55831642+charliefil@users.noreply.github.com>
2021-04-14 13:07:19 +0200

    councilbox

---
+++ component/Layout.js
@@ -0,0 +1,20 @@
+
+import Head from "next/head";
+
+import Nav from "./Navbar";
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title> {title} </title>
+      </Head>
+
+      <Nav />
+
+      {children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGO_URI: "",      // MongoDB's URI goes here
+    SECRET: "",         // SECRET key goes here
+};
